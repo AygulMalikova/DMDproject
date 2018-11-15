04261691ccc975c87d2b8c6ac1daaacc6f5696d6
plohoicod <35569768+plohoicod@users.noreply.github.com>
2018-11-15 20:41:15 +0300

    small changes

--- core/models.py
+++ core/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Customer(models.Model):
-    username = models.CharField(max_length=10, primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.TextField(default="")
-    country = models.TextField(default="")
-    zipcode = models.CharField(max_length=30)
-    city = models.TextField(default="")
-    phone_number = models.TextField(default="")
 
 class Employee(models.Model):
     username = models.CharField(max_length=10, primary_key=True)
@@ -18,22 +9,10 @@ class Employee(models.Model):
     email = models.TextField(default="")
     phone_number = models.TextField(default="")
 
-class ProvidngManager(models.Model):
-    companyid = models.AutoField(primary_key=True)
-    country = models.TextField(default="")
-    zipcode = models.CharField(max_length=30)
-    city = models.TextField(default="")
-    name_of_the_provider = models.TextField(default="")
 
-# class Operator(models.Model):
+class VehicleEngineer(models.Model):
+    info = models.ForeignKey(Employee, on_delete=models.CASCADE)
 
-# class VehicleEngineer(models.Model):
-
-class Car(models.Model):
-    car_id = models.AutoField(primary_key=True)
-    model = models.TextField(default="")
-    amount_of_places = models.IntegerField(default=4)
-    color = models.TextField(default="white")
 
 class Workshop(models.Model):
     workshopid = models.AutoField(primary_key=True)
@@ -41,11 +20,23 @@ class Workshop(models.Model):
     zipcode = models.CharField(max_length=30)
     city = models.TextField(default="")
 
+
+class ProvidngManager(models.Model):
+    companyid = models.AutoField(primary_key=True)
+    country = models.TextField(default="")
+    zipcode = models.CharField(max_length=30)
+    city = models.TextField(default="")
+    name_of_the_provider = models.TextField(default="")
+    info = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE)
+
 class CarParts(models.Model):
     type = models.TextField(default="")
     car_model = models.TextField(default="")
     count = models.IntegerField(default=0)
     color = models.TextField(default="white")
+    engineer = models.ForeignKey(VehicleEngineer, on_delete=models.CASCADE)
+    workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE)
 
 class ChargingStation(models.Model):
     uid = models.AutoField(primary_key=True)
@@ -59,6 +50,34 @@ class VehiclePark(models.Model):
     vid = models.AutoField(primary_key=True)
     location = models.TextField(default="")
     amount_of_cars = models.IntegerField(default=0)
+
+class Car(models.Model):
+    car_id = models.AutoField(primary_key=True)
+    model = models.TextField(default="")
+    amount_of_places = models.IntegerField(default=4)
+    color = models.TextField(default="white")
+    engineer = models.ForeignKey(VehicleEngineer, on_delete=models.CASCADE)
+    park = models.ForeignKey(VehicleEngineer, on_delete=models.CASCADE)
+    charging_station = models.ForeignKey(ChargingStation, on_delete=models.CASCADE)
+
+class Operator(models.Model):
+    parks = models.ManyToManyField(VehiclePark)
+    cars = models.ManyToManyField(Car)
+    info = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+class Customer(models.Model):
+    username = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.TextField(default="")
+    country = models.TextField(default="")
+    zipcode = models.CharField(max_length=30)
+    city = models.TextField(default="")
+    phone_number = models.TextField(default="")
+    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    car = models.OneToOneField(Car, on_delete=models.CASCADE)
+
+
 
     def __unicode__(self):
         return u"%s" % self.model
