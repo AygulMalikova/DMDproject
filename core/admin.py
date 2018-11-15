095ee602b7c588ffac40f808fd2d1daae20ced83
Almir Mullanurov <almirka02261999@gmail.com>
2018-11-15 21:23:52 +0300

    Tables with connections

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from core import models
 
+admin.site.register(models.Employee)
+admin.site.register(models.VehicleEngineer)
+admin.site.register(models.Workshop)
+admin.site.register(models.ProvidngManager)
+admin.site.register(models.CarParts)
+admin.site.register(models.ChargingStation)
+admin.site.register(models.VehiclePark)
 admin.site.register(models.Car)
+admin.site.register(models.Operator)
+admin.site.register(models.Customer)
